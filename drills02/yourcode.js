cb63b30330f3d241d033ca4bdcfb2e19274f0380
alexfrompalmsprings <alexavila.oc@gmail.com>
2020-04-05 12:12:49 -0700

    pass getMinMaxMean ✅

--- drills02/yourcode.js
+++ drills02/yourcode.js
@@ -13,13 +13,36 @@ function countOccurences(arr, word){
 }
 // console.log(countOccurences(['dog','cat','dog','pig','canary','cat','dog'],'dog'));
 
-function wordLengths(){
+function wordLengths(arr){
 
 }
 
-function getMinMaxMean(){
 
+
+function getMinMaxMean(arr){
+  let sorted = arr.sort((a,b) => (a -b));
+  let sum = 0;
+  for(let i =0; i < sorted.length; i++){
+    let current = sorted[i]
+
+    sum = sum + current;
+  }
+
+
+  let max = sorted[sorted.length -1]
+  let min = sorted[0]
+  let avg = sum / (sorted.length)
+
+  console.log(`here we have the min ${min}, the max ${max} && the mean ${avg}`)
+
+  let result = {}
+  result.min = min;
+  result.max = max;
+  result.mean = avg;
+
+  return result;
 }
+console.log(getMinMaxMean( [5,-5,15,25,12,2]))
 
 function findMode(){
 
