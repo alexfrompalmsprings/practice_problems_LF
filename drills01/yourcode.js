a37a19cb5b3f935cbf9d8b5364122910eecb2d90
alexfrompalmsprings <alexavila.oc@gmail.com>
2020-04-04 22:09:57 -0700

    init

---
+++ drills01/yourcode.js
@@ -0,0 +1,29 @@
+function sumArray(arr) {
+
+
+}
+
+// console.log(sumArray([4,2,3,6,7,1,8,10,9]));
+
+function fitWithinVal(arr, maxValue) {
+
+}
+
+// console.log(fitWithinVal([4,2,3,6,7,1,8,10,9], 30));
+// console.log(fitWithinVal([24,49,63,6,79,65,60,86,94,36,40,29,42,25,12,4,83,88,4,72], 500))
+
+
+function getAllNamesShorterThan(arr, limitOfChars) {
+
+}
+
+
+// console.log(getAllNamesShorterThan(['Clyde','Sue','Jerry','Jo'], 4));
+
+function makeLabel(arr) {
+
+
+}
+
+
+
